17b78d2481e126ec239166a2d14729a7f2a62cf1
wendymejia <wendyvanessa.ma@gmail.com>
2023-12-20 20:30:36 -0500

    Se realiza configuración de los archivos del gradle con Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.devexpert"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.devexpert"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    viewBinding{
+        android.buildFeatures.viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility (JavaVersion.VERSION_17)
+        targetCompatibility (JavaVersion.VERSION_17)
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation ("androidx.core:core-ktx:1.8.0")
+    implementation ("androidx.appcompat:appcompat:1.4.1")
+    implementation ("com.google.android.material:material:1.5.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation ("androidx.recyclerview:recyclerview:1.3.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
+    implementation ("com.github.bumptech.glide:glide:4.11.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.5")
+    kapt ("com.github.bumptech.glide:compiler:4.11.0")
+
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "devExpert"
-include ':app'
+include (":app")
